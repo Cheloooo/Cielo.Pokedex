36f27d2f98810037d7273248d5e225177393ee8a
Cheloooo <50958722+Cheloooo@users.noreply.github.com>
2024-07-28 17:29:05 +0800

    additional design

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-let arr = []
+let arr = [];
 const init = () => {
     fetchAllPokemon();
 };
-// let us query all pokemon names
 
+const searchBar = document.getElementById('searchBar');
+console.log(searchBar);
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+    console.log(searchString);
+    const filterPokemon = arr.filter ((pokemon) => {
+     return (
+        pokemon.name.toLowerCase().includes(searchString) || 
+        pokemon.types.includes(searchString)
+     );
+    });
+    showPokemon(filterPokemon);
+});
+
+// let us query all pokemon names
 const fetchAllPokemon = async() => {
     let res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
   //response from this fetch to be retured
@@ -31,9 +45,13 @@ const showPokemon = (pokemon) =>{
     const output = document.querySelector('.output')
     const map = pokemon.map(each =>{
         return `
-        <div>
+        <div class="poke-card">
             <h1>${each.name}</h1>
             <img src=${each.sprites.front_default} alt=${each.name}/>
+            <div class="poke-types">
+                <span>${each.types[0].type.name}</span>
+                <span>${each.types.length > 1 ? each.types[1].type.name : ''}</span>
+            </div>
         </div>
         `
     }).join('');
