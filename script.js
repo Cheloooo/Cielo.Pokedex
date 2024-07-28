f6f9f8d7c070eaa80bdb88fcc9ab69564c03ee13
Cheloooo <50958722+Cheloooo@users.noreply.github.com>
2024-07-28 14:09:23 +0800

    changed code

--- script.js
+++ script.js
@@ -1,85 +1,35 @@
-let pokemons = [];
-const poke_container = document.getElementById("poke_container");
-const url = "https://pokeapi.co/api/v2/pokemon";
-const pokemons_number = 151;
-const search = document.getElementById("search");
-const form = document.getElementById("form");
 
+const pokedex = document.getElementById("pokedex");
 
-const fetchPokemons = async () => {
-    for (let i = 1; i<= pokemons_number; i++){
-        await getAllPokemon(i);
+console.log(pokedex);
+const fetchPokemon = () => {
+    const promises =[];
+    for (let i = 1; i <= 150; i++){
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
     }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((data) => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map((type) => type.type.name).join(', ')
+        }));
+        displayPokemon(pokemon);
+    });
 };
 
-const removePokemon = () => {
-    const pokemonEls = document.getElementByClassName("pokemon");
-    let removablePokemons = [];
-    for (let i=0; i < pokemonEls.length; i++){
-        const pokemonEl = pokemonEls[i];
-        removablePokemons = [...removablePokemons, pokemonEl];
-    }
-    removablePokemons.forEach((remPoke) => remPoke.remove());
-};
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon.map((pokeman) =>`
+        <p>
+        <img src="${pokeman.image} />
+        <h2>${pokeman.id}. ${pokeman.name}</h2>
+        <p>Type: ${pokeman.type}</p>
 
-const getPokemon = async (id) => {
-    const searchPokemons = pokemons.filter((poke) = poke.name === id);
-    removePokemon();
-    searchPokemons.forEach((pokemon) => createPokemonCard(pokemon));
-};
-
-const getAllPokemon = async (id) => {
-    const res = await fetch(`${url}/${id}`);
-    const pokemon = await res.json();
-    pokemons = [...pokemons,pokemon];
-};
-
-fetchPokemons();
-
-function createPokemonCard(pokemon){
-    const pokemonEl = document.createElement("div");
-    pokemonEl.classList.add("pokemon");
-    const poke_types = pokemon.types.map((el) => el.type.name).slice(0,1);
-    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-    const poke_stat = pokemon.stats.map((el) => el.stat.name);
-    const stats = poke_stat.slice(0,3);
-    const base_value = pokemon.stats.map((el) => e.base_stat);
-    const base_stat = base_value.slice(0,3);
-    const stat = stats.map((stat) =>{
-        return `<li class = "names"> ${stat}</li>`;
-    }).join("");
-    const base = base_stat.map((base) => {
-        return `<li class="base">${base}</li>`
-    }).join("");
-    const pokeInnerHTML = `<div class="img-container">
-    <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="${name}"/>
-    </div>
-    <div class="infor">
-    <spanclass = "number">#${pokemon.id.toString().padStar(3,"0")}</span>
-    <h3 class = "name">${name}</h3>
-    <small class="type"><span>${poke_types}</span></small>
-    </div>
-    <div class="stats">
-    <h2>Stats</h2>
-    <div class="flex">
-    <ul>${stat}</ul>
-    <ul>${base}</ul>
-    </div>
-    </div>
-    `;
-    pokemonEl.innerHTML = pokeInnerHTML;
-    poke_container.appendChild(pokemonEl);
+        </p>
+        `).join('');
+    pokedex.innerHTML = pokemonHTMLString;
 }
 
-form.addEventListener("submit", (e) =>{
-    e.preventDefault();
-    const searchItem = search.nodeValue;
-    if (searchItem){
-        getPokemon(searchItem);
-        search.value = "";
-    } else if (searchItem === ""){
-        pokemons = [];
-        removePokemon();
-        fetchPokemons();
-    }
-});
+fetchPokemon();
